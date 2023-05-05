00af55f240d2b8039e7d8f7fd47cefd5a41c1c17
Ssekyanzi-Joel <105359998+Ssekyanzi-Joel@users.noreply.github.com>
2023-05-05 11:12:28 +0300

    adding Calender

--- PNYNE/forms.py
+++ PNYNE/forms.py
@@ -1,8 +1,11 @@
-from django import forms
+from django import forms, DateInput
 from .models import Task
+
 
 class NewTask(forms.ModelForm):
     class Meta:
-        model =Task
-        fields = 'title','description','complete','due_date','priority','completed_date','tags'
-        
+        model = Task
+        fields = 'title', 'description', 'complete', 'due_date', 'priority', 'completed_date', 'tags'
+        widgets = {
+            'due_date': DateInput(attrs={'type': 'date'}),
+        }
